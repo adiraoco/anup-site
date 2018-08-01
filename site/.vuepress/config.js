3714a8e13f3e4114ff624ee03366d18de0891e01
adiraomj <adiraomj@outlook.com>
2018-08-01 10:17:51 +0530

    site with vuepress

---
+++ site/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Anup\'s Site',
+  description: "A simple site",
+  themeConfig:{
+    nav: [
+      { text: 'HOME', link: '/' },
+      { text: 'ABOUT', link: '/about/' },
+      { text: 'SERVICES', link: '/services/' },
+      { text: 'CLASSES', link: '/classes/' },
+      { text: 'SCHEDULE', link: '/schedule/' },
+      { text: 'GALLERY', link: '/gallery/' },
+      { text: 'CONTACT', link: '/contact/' },
+    ]
+  }
+}
